25cbccdc4bc387e714b1303fa5edc0b0c7a5e1c7
sennafilipe42 <filipesennati@gmail.com>
2021-08-31 19:44:04 -0300

    Dapper rodando e comunicando com o Banco de Dados

---
+++ SRC/Infra/DbConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using Domain.Entities;
+
+namespace Infra
+{
+    public class DbConnection
+    {
+
+        static void Main(string[] args)
+        {
+
+            var stringConexao = "Server= localhost; Database= senna; Integrated Security=True;";
+
+            var sql = "SELECT TOP 10 * FROM TREINA_USUARIOS";
+
+            var sqlConnection = new SqlConnection(stringConexao);
+            var usuarios = sqlConnection.Query<User>(sql);
+
+            foreach (var u in usuarios)
+            {
+                Console.WriteLine(u.Nome);
+
+            }
+
+        }
+    }
+}
